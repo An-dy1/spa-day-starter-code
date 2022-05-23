a2f779299cf651695cfc0f879d3503c27cd8075d
Andie Johnson <ajohnson2@naic.org>
2022-05-23 13:14:56 -0500

    did one spa day part one studio bonus

--- src/main/java/org/launchcode/spaday/controllers/SpaDayController.java
+++ src/main/java/org/launchcode/spaday/controllers/SpaDayController.java
@@ -5,24 +5,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.ArrayList;
 
 
 @Controller
 public class SpaDayController {
 
-    @GetMapping
-    public String customerForm () {
-        return "serviceSelection";
-    }
+	@GetMapping
+	public String customerForm() {
+		return "serviceSelection";
+	}
 
-    @PostMapping
-    public String spaMenu(@RequestParam String skintype, @RequestParam String manipedi, Model model) {
+	@PostMapping("menu")
+	public String spaMenu(@RequestParam String skintype, @RequestParam String manipedi, Model model) {
 
-        Client newClient = new Client(skintype, manipedi);
-        newClient.setAppropriateFacials(skintype);
-        model.addAttribute("client" , newClient);
+		Client newClient = new Client(skintype, manipedi);
+		newClient.setAppropriateFacials(skintype);
+		model.addAttribute("client", newClient);
 
-        return "menu";
-    }
+		return "menu";
+	}
+
 }
