b04fea3174d43f4d635b5bf51f395be1760993e3
Andie Johnson <ajohnson2@naic.org>
2022-05-26 13:23:53 -0500

    first section of studio - add validation annotation

--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,42 +1,58 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 public class User {
-    private String username;
-    private String email;
-    private String password;
 
-    public User() {
+	@NotBlank(message = "username cannot be blank")
+	@Size(min = 5, max = 15, message = "username must be between 5 and 15 characters")
+	private String username;
 
-    }
+	@Email
+	private String email;
 
-    public User(String username, String email, String password) {
-        this();
-        this.username = username;
-        this.email = email;
-        this.password = password;
-    }
+	@Min(value = 6, message = "password must be at least 6 characters")
+	@NotBlank(message = "password cannot be blank")
+	private String password;
 
-    public String getUsername() {
-        return username;
-    }
+	@NotBlank(message = "verify password cannot be blank")
+	private String verify;
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
+	public User() {
 
-    public String getEmail() {
-        return email;
-    }
+	}
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+	public User(String username, String email, String password) {
+		this();
+		this.username = username;
+		this.email = email;
+		this.password = password;
+	}
 
-    public String getPassword() {
-        return password;
-    }
+	public String getUsername() {
+		return username;
+	}
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 }
